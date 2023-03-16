2d7d48015a9a8819979930be87ff482fdcb47052
Agm567 <detolsabum@gmail.com>
2023-03-16 14:49:11 +0700

    first commit

---
+++ pertemuan3/tugas3/home.php
@@ -0,0 +1,16 @@
+<?php
+
+	include_once "header.php"
+
+?>
+
+				<h1>Welcome Home!</h1>
+				<p>
+                    Agung Gunara Ma'mun - 0110122115 - SI05
+				</p>
+				
+<?php
+
+include_once "footer.php"
+
+?>
